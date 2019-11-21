9d4e37efc0e582e7f1017945f8145bec7cc0469b
Leandro Gioria <leandro.gioria@Leandros-MacBook-Air.local>
2019-11-21 01:27:46 -0300

    Fixed formatation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { MdShoppingBasket } from 'react-icons/md';
-import { Container , Cart } from './styles';
+import { Container, Cart } from './styles';
 import logo from '../../assets/images/logo.svg';
-
-
 
 export default function Header() {
     return (
