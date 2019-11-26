e179030e917f2d49ce23861f751e6ec8973fb795
Leandro Gioria <legioria@hotmail.com>
2019-11-26 18:35:21 -0300

    Implemented middleware for redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,22 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
+import rootSaga from './modules/rootSaga';
 import rootReducer from './modules/rootReducer';
+
+const sagaMiddleware = createSagaMiddleware();
 
 // Reactotron config console
 const enhancer =
     process.env.NODE_ENV === 'development'
-        ? console.tron.createEnhancer()
-        : null;
+        ? compose(
+              console.tron.createEnhancer(),
+              applyMiddleware(sagaMiddleware)
+          )
+        : applyMiddleware(sagaMiddleware);
 
 const store = createStore(rootReducer, enhancer);
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
